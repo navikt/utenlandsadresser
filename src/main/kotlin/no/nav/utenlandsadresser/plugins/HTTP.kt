f6182068ca37a9ce3a8f41c79a0713995913fa24
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-12-07 11:08:27 +0100

    Init project

---
+++ src/main/kotlin/no/nav/utenlandsadresser/plugins/HTTP.kt
@@ -0,0 +1,11 @@
+package no.nav.utenlandsadresser.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    routing {
+        openAPI(path = "openapi")
+    }
+}
---
+++ src/main/kotlin/no/nav/utenlandsadresser/plugins/Security.kt
@@ -0,0 +1,6 @@
+package no.nav.utenlandsadresser.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+}
