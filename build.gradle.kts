a520baad00c8a97bc0daf17c67f323d0e4a6a6d1
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2023-12-18 14:29:45 +0100

    Bump transitive dependency guava

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,11 @@ dependencies {
 
     implementation("io.ktor:ktor-server-openapi:$ktorVersion")
     constraints {
+        // Transitive dependencies of ktor-server-openapi
         implementation("org.json:json:20231013") {
+            because("Previous versions have security vulnerabilities")
+        }
+        implementation("com.google.guava:guava:32.1.3-android") {
             because("Previous versions have security vulnerabilities")
         }
     }
