56ba88594fe644abcd5167dd81af384e5bd87d04
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2024-03-05 13:21:08 +0100

    Bruk GenericRecord under deserialisering

--- src/main/kotlin/no/nav/utenlandsadresser/infrastructure/client/kafka/Avro4kLivshendelseDeserializer.kt
+++ src/main/kotlin/no/nav/utenlandsadresser/infrastructure/client/kafka/Avro4kLivshendelseDeserializer.kt
@@ -6,6 +6,9 @@ import org.apache.kafka.common.serialization.Deserializer
 
 class Avro4kLivshendelseDeserializer : Deserializer<LivshendelseAvro> {
     private val avro = Avro(AvroConfiguration(implicitNulls = true))
-    override fun deserialize(topic: String?, data: ByteArray): LivshendelseAvro =
-        avro.decodeFromByteArray(LivshendelseAvro.serializer(), data)
+    override fun deserialize(topic: String?, data: ByteArray): LivshendelseAvro {
+        return avro.openInputStream(LivshendelseAvro.serializer())
+            .from(data)
+            .nextOrThrow()
+    }
 }
