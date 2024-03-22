4d4f855a7d35aa33cbe14b76cd5333e6889de010
Odd Gunnar Fatland <odd.gunnar.fatland@nav.no>
2024-03-22 10:40:01 +0100

    Legg til eksempler i Swagger

--- src/main/kotlin/no/nav/utenlandsadresser/infrastructure/route/json/StoppAbonnementJson.kt
+++ src/main/kotlin/no/nav/utenlandsadresser/infrastructure/route/json/StoppAbonnementJson.kt
@@ -1,8 +1,10 @@
 package no.nav.utenlandsadresser.infrastructure.route.json
 
+import io.github.smiley4.ktorswaggerui.dsl.Example
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class StoppAbonnementJson(
+    @Example("f47b4b9d-3f6d-4f3e-8f2d-3f4b4f3e2d1f")
     val abonnementId: String,
 )
