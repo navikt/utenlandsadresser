967973c453a8194ec7ed5e8eadd31aa96b9c3531
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-05-07 12:57:42 +0200

    Legg på Logback-metrikker

--- src/main/kotlin/no/nav/utenlandsadresser/plugin/Metrics.kt
+++ src/main/kotlin/no/nav/utenlandsadresser/plugin/Metrics.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.micrometer.core.instrument.binder.logging.LogbackMetrics
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
@@ -12,6 +13,7 @@ fun Application.configureMetrics() {
 
     install(MicrometerMetrics) {
         registry = appMicrometerRegistry
+        meterBinders += LogbackMetrics()
     }
 
     routing {
