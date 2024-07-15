8f2908463c89a3cc1a9f9c829578d7fdff0b976d
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-07-15 11:28:50 +0200

    Set opp CI/CD for cleanup job

--- sporingslogg-cleanup/build.gradle.kts
+++ sporingslogg-cleanup/build.gradle.kts
@@ -1,19 +1,20 @@
 plugins {
     kotlin("jvm")
     idea
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass = "no.nav.utenlandsadresser.sporingslogg.cleanup.MainKt"
 }
 
 dependencies {
     implementation(project(":app"))
     implementation(libs.bundles.ktorClient)
     implementation(libs.bundles.hoplite)
-
-    testImplementation(testLibs.bundles.kotest)
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
--- sporingslogg-cleanup/src/test/kotlin/no/nav/utenlandsadresser/sporingslogg/cleanup/kotest/KotestProjectConfig.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.utenlandsadresser.sporingslogg.cleanup.kotest
-
-import io.kotest.core.config.AbstractProjectConfig
-
-object KotestProjectConfig : AbstractProjectConfig() {
-    override val parallelism = Runtime.getRuntime().availableProcessors()
-    override val autoScanEnabled = false
-}
