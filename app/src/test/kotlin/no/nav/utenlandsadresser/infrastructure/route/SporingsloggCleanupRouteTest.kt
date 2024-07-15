e957a5c34f705bad68606ec89540df665edc236c
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-07-15 10:12:47 +0200

    Legg til sporingslogg cleanup route i app

---
+++ app/src/test/kotlin/no/nav/utenlandsadresser/infrastructure/route/SporingsloggCleanupRouteTest.kt
@@ -0,0 +1,53 @@
+package no.nav.utenlandsadresser.infrastructure.route
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+import io.ktor.client.request.delete
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
+import io.mockk.coEvery
+import io.mockk.mockk
+import no.nav.utenlandsadresser.infrastructure.persistence.postgres.SporingsloggPostgresRepository
+import no.nav.utenlandsadresser.kotest.extension.specWideTestApplication
+import no.nav.utenlandsadresser.util.years
+
+class SporingsloggCleanupRouteTest :
+    WordSpec({
+        val sporingsloggPostgresRepository = mockk<SporingsloggPostgresRepository>()
+
+        val client =
+            specWideTestApplication {
+                application {
+                    routing {
+                        route("/internal") {
+                            configureSporingsloggCleanupRoute(sporingsloggPostgresRepository)
+                        }
+                    }
+                }
+            }.client
+
+        "DELETE /internal/sporingslogg" should {
+            "return bad request when query param is missing" {
+                val response = client.delete("/internal/sporingslogg")
+
+                response.status shouldBe HttpStatusCode.BadRequest
+            }
+
+            "return bad request when query param is invalid" {
+                val response = client.delete("/internal/sporingslogg?olderThan=NotISO8601Duration")
+
+                response.status shouldBe HttpStatusCode.BadRequest
+            }
+
+            "return ok when query param is valid and sporingslogg older than the duration is deleted" {
+                coEvery { sporingsloggPostgresRepository.deleteSporingsloggerOlderThan(any()) } returns Unit
+
+                val duration = 10.years.toIsoString()
+
+                val response = client.delete("/internal/sporingslogg?olderThan=$duration")
+
+                response.status shouldBe HttpStatusCode.OK
+            }
+        }
+    })
