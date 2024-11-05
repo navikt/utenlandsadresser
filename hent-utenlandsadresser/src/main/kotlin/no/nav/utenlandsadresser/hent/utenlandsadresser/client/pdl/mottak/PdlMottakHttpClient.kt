475876c9ca1e5ba6f6bd320b6da7a24bdf9fef61
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-11-05 13:09:24 +0100

    Logg respons

--- hent-utenlandsadresser/src/main/kotlin/no/nav/utenlandsadresser/hent/utenlandsadresser/client/pdl/mottak/PdlMottakHttpClient.kt
+++ hent-utenlandsadresser/src/main/kotlin/no/nav/utenlandsadresser/hent/utenlandsadresser/client/pdl/mottak/PdlMottakHttpClient.kt
@@ -3,6 +3,7 @@ package no.nav.utenlandsadresser.hent.utenlandsadresser.client.pdl.mottak
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import kotlinx.datetime.Clock
@@ -17,43 +18,49 @@ import no.nav.utenlandsadresser.hent.utenlandsadresser.client.pdl.mottak.json.Op
 import no.nav.utenlandsadresser.hent.utenlandsadresser.client.pdl.mottak.json.PersonendringJson
 import no.nav.utenlandsadresser.hent.utenlandsadresser.client.pdl.mottak.json.PersonopplysningJson
 import no.nav.utenlandsadresser.hent.utenlandsadresser.client.pdl.mottak.json.UtenlandskAdresseJson
+import org.slf4j.LoggerFactory
 import java.net.URL
 
 class PdlMottakHttpClient(
     private val httpClient: HttpClient,
     private val baseUrl: URL,
 ) : OppdaterUtenlandsadresse {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     override suspend fun oppdaterUtenlandsadresse(
         identitetsnummer: String,
         utenlandskAdresse: UtenlandskAdresseJson,
     ) {
-        httpClient.post("$baseUrl/api/v1/endringer") {
-            contentType(ContentType.Application.Json)
-            val iDag =
-                Clock.System
-                    .now()
-                    .toLocalDateTime(TimeZone.currentSystemDefault())
-                    .date
-            setBody(
-                PersonendringJson(
-                    listOf(
-                        PersonopplysningJson(
-                            endringstype = EndringstypeJson.OPPRETT,
-                            ident = identitetsnummer,
-                            opplysningstype = OpplysningstypeJson.UTENLANDSK_KONTAKTADRESSE,
-                            endringsmelding =
-                                EndringsmeldingJson.Kontaktadresse(
-                                    kilde = "Skatteetaten",
-                                    gyldigFraOgMed = iDag,
-                                    gyldigTilOgMed = iDag.plus(1, DateTimeUnit.YEAR),
-                                    coAdressenavn = null,
-                                    adresse = utenlandskAdresse,
-                                ),
-                            opplysningsId = null,
+        httpClient
+            .post("$baseUrl/api/v1/endringer") {
+                contentType(ContentType.Application.Json)
+                val iDag =
+                    Clock.System
+                        .now()
+                        .toLocalDateTime(TimeZone.currentSystemDefault())
+                        .date
+                setBody(
+                    PersonendringJson(
+                        listOf(
+                            PersonopplysningJson(
+                                endringstype = EndringstypeJson.OPPRETT,
+                                ident = identitetsnummer,
+                                opplysningstype = OpplysningstypeJson.UTENLANDSK_KONTAKTADRESSE,
+                                endringsmelding =
+                                    EndringsmeldingJson.Kontaktadresse(
+                                        kilde = "Skatteetaten",
+                                        gyldigFraOgMed = iDag,
+                                        gyldigTilOgMed = iDag.plus(1, DateTimeUnit.YEAR),
+                                        coAdressenavn = null,
+                                        adresse = utenlandskAdresse,
+                                    ),
+                                opplysningsId = null,
+                            ),
                         ),
                     ),
-                ),
-            )
-        }
+                )
+            }.also {
+                logger.info("Response from PDL-mottak: ${it.bodyAsText()}")
+            }
     }
 }
