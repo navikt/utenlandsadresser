df6bc10867cbb214714da459ed2f3d4bb9eb0a9f
Odd Gunnar <oddgunnarfatland@gmail.com>
2024-11-27 12:51:06 +0100

    Nytt format på CallLogging

--- app/src/main/kotlin/no/nav/utenlandsadresser/plugin/CallLogging.kt
+++ app/src/main/kotlin/no/nav/utenlandsadresser/plugin/CallLogging.kt
@@ -3,6 +3,10 @@ package no.nav.utenlandsadresser.plugin
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.plugins.calllogging.processingTimeMillis
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.path
+import kotlinx.datetime.Clock
 import org.slf4j.LoggerFactory
 import org.slf4j.event.Level
 
@@ -10,5 +14,16 @@ fun Application.configureCallLogging() {
     install(CallLogging) {
         level = Level.INFO
         logger = LoggerFactory.getLogger("CallLogging")
+        format { call ->
+            val method = call.request.httpMethod.value
+            val path = call.request.path()
+            val status = call.response.status()
+            val processingTime =
+                call.processingTimeMillis {
+                    Clock.System.now().toEpochMilliseconds()
+                }
+
+            "$method $path $status ${processingTime}ms"
+        }
     }
 }
