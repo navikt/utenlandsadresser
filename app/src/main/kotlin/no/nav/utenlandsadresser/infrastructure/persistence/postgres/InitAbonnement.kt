9d9eff1b2007e3f8259d9c71215e3e5048bdc676
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-01-10 15:29:04 +0100

    Alltid send utenlandsadresse på feeden når man prøver å opprette abonnement

--- app/src/main/kotlin/no/nav/utenlandsadresser/infrastructure/persistence/postgres/InitAbonnement.kt
+++ app/src/main/kotlin/no/nav/utenlandsadresser/infrastructure/persistence/postgres/InitAbonnement.kt
@@ -2,13 +2,12 @@ package no.nav.utenlandsadresser.infrastructure.persistence.postgres
 
 import arrow.core.Either
 import arrow.core.getOrElse
-import arrow.core.raise.either
 import kotlinx.coroutines.Dispatchers
+import no.nav.utenlandsadresser.app.AbonnementInitializer
 import no.nav.utenlandsadresser.domain.Abonnement
 import no.nav.utenlandsadresser.domain.FeedEvent
 import no.nav.utenlandsadresser.domain.Hendelsestype
 import no.nav.utenlandsadresser.domain.Postadresse
-import no.nav.utenlandsadresser.app.AbonnementInitializer
 import no.nav.utenlandsadresser.infrastructure.persistence.CreateAbonnementError
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
@@ -16,6 +15,13 @@ class PostgresAbonnementInitializer(
     private val abonnementRepository: PostgresAbonnementRepository,
     private val feedRepository: PostgresFeedRepository,
 ) : AbonnementInitializer {
+    /**
+     * Oppretter et abonnement i databasen hvis det ikke allerede finnes.
+     * Hvis et abonnement allerede finnes, så returneres det eksisterende abonnementet.
+     *
+     * Uansett om abonnementet finnes eller ikke, så opprettes et feed event hvis det finnes en utenlandsk adresse.
+     * Dette er for å forsikre at alle abonnementer får en feed event når de opprettes.
+     */
     override suspend fun initAbonnement(
         abonnement: Abonnement,
         postadresse: Postadresse?,
@@ -27,32 +33,28 @@ class PostgresAbonnementInitializer(
         with(abonnementRepository) {
             with(feedRepository) {
                 newSuspendedTransaction(Dispatchers.IO) {
-                    either {
-                        val createdAbonnement =
-                            createAbonnement(abonnement).getOrElse {
-                                when (it) {
-                                    is CreateAbonnementError.AlreadyExists ->
-                                        raise(
-                                            InitAbonnementError.AbonnementAlreadyExists(it.abonnement),
-                                        )
-                                }
+                    val createAbonnementResult =
+                        createAbonnement(abonnement).mapLeft {
+                            when (it) {
+                                is CreateAbonnementError.AlreadyExists ->
+                                    InitAbonnementError.AbonnementAlreadyExists(it.abonnement)
                             }
-
-                        if (postadresse is Postadresse.Utenlandsk) {
-                            val feedEvent =
-                                FeedEvent.Incoming(
-                                    identitetsnummer = abonnement.identitetsnummer,
-                                    abonnementId = createdAbonnement.id,
-                                    organisasjonsnummer = abonnement.organisasjonsnummer,
-                                    hendelsestype = Hendelsestype.OppdatertAdresse,
-                                )
-                            createFeedEvent(feedEvent)
                         }
 
-                        createdAbonnement
-                    }.onLeft {
-                        rollback()
+                    val abonnementFromRepository = createAbonnementResult.getOrElse { it.abonnement }
+
+                    if (postadresse is Postadresse.Utenlandsk) {
+                        val feedEvent =
+                            FeedEvent.Incoming(
+                                identitetsnummer = abonnement.identitetsnummer,
+                                abonnementId = abonnementFromRepository.id,
+                                organisasjonsnummer = abonnement.organisasjonsnummer,
+                                hendelsestype = Hendelsestype.OppdatertAdresse,
+                            )
+                        createFeedEvent(feedEvent)
                     }
+
+                    createAbonnementResult
                 }
             }
         }
