c1067919ecdaafa30934fba58bfef56f33189f90
Odd Gunnar <odd.gunnar.fatland@nav.no>
2026-08-26 14:29:25 +0200

    Sjekk etter Nav-Reason-Code i header for å oppdage feilmelding på person med falsk identitet (#415)
    * Sjekk etter Nav-Reason-Code i header for å oppdage feilmelding på person med falsk identitet
    
    * Legg til test for å verifisere at 409 uten header fremdeles returnerer ukjent feil

--- app/src/test/kotlin/no/nav/utenlandsadresser/infrastructure/client/http/regoppslag/RegOppslagHttpClientTest.kt
+++ app/src/test/kotlin/no/nav/utenlandsadresser/infrastructure/client/http/regoppslag/RegOppslagHttpClientTest.kt
@@ -194,7 +194,21 @@ class RegOppslagHttpClientTest :
                     .shouldBeTypeOf<GetPostadresseError.UkjentAdresse>()
             }
 
-            "return falsk identitet error when response status is 409" {
+            "return falsk identitet error when response status is 409 with corresponding header" {
+                mockServer.post {
+                    url equalTo "/rest/postadresse"
+                } returns {
+                    statusCode = HttpStatusCode.Conflict.value
+                    header = "Nav-Reason-Code" to "falsk_identitet"
+                }
+
+                regOppslagHttpClient
+                    .getPostadresse(identitetsnummer)
+                    .leftOrNull()
+                    .shouldBeTypeOf<GetPostadresseError.FalskIdentiet>()
+            }
+
+            "return ukjent feil error when response status is just 409" {
                 mockServer.post {
                     url equalTo "/rest/postadresse"
                 } returns {
@@ -204,7 +218,7 @@ class RegOppslagHttpClientTest :
                 regOppslagHttpClient
                     .getPostadresse(identitetsnummer)
                     .leftOrNull()
-                    .shouldBeTypeOf<GetPostadresseError.FalskIdentiet>()
+                    .shouldBeTypeOf<GetPostadresseError.UkjentFeil>()
             }
 
             "return teknisk error when response status is 500" {
